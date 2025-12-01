f956fbe10a2b438dec2c98d07f6fb9b67177959a
javier-albino <javier.sandoval.temuco@gmail.com>
2025-12-01 11:39:30 -0300

    hot fix token jwt  for cobayas swagger

--- src/main/java/com/irojas/demojwt/Config/OpenApiConfig.java
+++ src/main/java/com/irojas/demojwt/Config/OpenApiConfig.java
@@ -3,7 +3,7 @@ package com.irojas.demojwt.Config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+#import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
