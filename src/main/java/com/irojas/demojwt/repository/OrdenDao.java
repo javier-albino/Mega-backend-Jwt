42aaac4d59cc5d8ce92f78454f53f4cddacd7a9f
javier-albino <javier.sandoval.temuco@gmail.com>
2025-12-01 17:34:55 -0300

    "add select"

--- src/main/java/com/irojas/demojwt/repository/OrdenDao.java
+++ src/main/java/com/irojas/demojwt/repository/OrdenDao.java
@@ -11,6 +11,4 @@ public interface OrdenDao extends JpaRepository<Orden, Long> {
     @Query("SELECT o FROM Orden o ORDER BY o.fechaCreacion ASC LIMIT 1")
     Orden findOldestOrden();
 
-    @Query("SELECT o FROM Orden o WHERE o.paciente.fechaNacimiento = (SELECT MIN(p.fechaNacimiento) FROM Paciente p) ORDER BY o.fechaCreacion DESC LIMIT 1")
-    Orden findNewestOrdenFromOldestPaciente();
 }
